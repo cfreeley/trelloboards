1d6025b943aa33ec8a189344c0aa0d5887a1b53d
Paul Ferrett <paul.ferrett@servicecentral.com.au>
2013-02-12 22:12:43 +1100

    Major improvements to Authorisation flow which is now done within a browser tab.

---
+++ authorise.js
@@ -0,0 +1,15 @@
+$(function() {
+	Trello.authorize({
+		'name':	"Boards for Trello",
+		'expiration': "never",
+		'success': function() {
+			// Close this window and open the popup
+			$('#auth').hide();
+			$('#success').show();
+		},
+		'error': function () {
+			$('#auth').hide();
+			$('#error').show();
+		}
+	});
+});
